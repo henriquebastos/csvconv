64d581cad9deebdd322a5538f7a5b51425fa5c50
Henrique Bastos <henrique@bastos.net>
2019-10-30 23:59:18 -0300

    v1.1

--- csvconv/__main__.py
+++ csvconv/__main__.py
@@ -5,11 +5,16 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-D', '--dialect', default='excel', choices=csv.list_dialects())
+    parser.add_argument('-D', '--dialect', choices=csv.list_dialects())
     parser.add_argument('-tD', '--to-dialect', default='excel-tab', choices=csv.list_dialects())
     parser.add_argument('INPUT', type=argparse.FileType())
 
     args = parser.parse_args()
+
+    if not args.dialect:
+        sniffer = csv.Sniffer()
+        args.dialect = sniffer.sniff(args.INPUT.read(1024))
+        args.INPUT.seek(0)
 
     with args.INPUT as f:
         reader = csv.reader(f, dialect=args.dialect)
